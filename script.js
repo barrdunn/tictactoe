f746f19e1783bc06cc3f712ac35e07d498ef79c9
Barry Dunn <barrydunn@Barrys-MacBook-Pro-2.local>
2020-12-30 21:28:20 -0700

    JS basics review

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Hello");
+const cellElements = document.querySelectorAll('[data-cell]');
+
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true })
+});
+
+function handleClick() {
+    console.log('clicked');
+}
+
