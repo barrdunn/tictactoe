943024155038d70e6942abae91a20f40333ecf0b
barrdunn <barrdunn@me.com>
2020-12-30 21:35:45 -0700

    JS review commit 2

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const cellElements = document.querySelectorAll('[data-cell]');
 
+let circleTurn
+
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick, { once: true })
 });
 
 function handleClick() {
-    console.log('clicked');
+    //Check for a mark
+    //Check for a win
+    //Check for a draw
+    //Switchturns
 }
 
