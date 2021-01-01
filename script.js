335c6e1dbbc6ad82e993d96e1004330583404b82
barrdunn <barrdunn@me.com>
2020-12-31 21:49:56 -0700

    X and O class logic

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-const cellElements = document.querySelectorAll('[data-cell]');
-
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
 let circleTurn
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+startGame()
 
-cellElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, { once: true })
-});
+function startGame() {
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true })
+    });
 
-function handleClick() {
+    setBoardHoverClass()
+}
+
+startGame()
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
     //Check for a mark
     //Check for a win
     //Check for a draw
-    //Switchturns
+    swapTurns()
+    setBoardHoverClass()
 }
 
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS)
+    } else {
+        board.classList.add(X_CLASS)
+    }
+}
